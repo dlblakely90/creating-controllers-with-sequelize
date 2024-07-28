5601637cd9f416808bd9a1026a80d902835ebf51
Dakota Lee <153787707+dlblakely90@users.noreply.github.com>
2024-07-28 18:31:03 -0400

    first commit

---
+++ models/band.js
@@ -0,0 +1,41 @@
+'use strict'
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Band extends Model {
+    
+    static associate(models) {
+      // define association here
+    }
+  }
+  Band.init({
+    band_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    genre: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    available_start_time: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    end_time: {
+        type: DataTypes.DATE,
+        allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Band',
+    tableName: 'bands',
+    timestamps: false
+  })
+  return Band
+}
